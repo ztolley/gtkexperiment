d7abbcc1623f991b128493e567752abd532d33c6
Zac Tolley <zac@thetolleys.com>
2021-08-31 13:43:39 +0100

    Sensible names for variables

--- main.c
+++ main.c
@@ -42,53 +42,53 @@ static void app_activate(GApplication *app, gpointer user_data) {
 
 static void app_open(GApplication *app, GFile **files, gint n_files, gchar *hint, gpointer user_data) {
     GtkWidget *win;
-    GtkWidget *nb;
-    GtkWidget *lab;
-    GtkNotebookPage *nbp;
-    GtkWidget *scr;
-    GtkWidget *tv;
-    GtkTextBuffer *tb;
+    GtkWidget *screen;
+    GtkWidget *notebook;
+    GtkNotebookPage *notebookPage;
+    GtkWidget *label;
+
+    GtkWidget *textView;
+    GtkTextBuffer *textBuffer;
     char *contents;
     gsize length;
-    char *filename;
-    int i;
-    GtkBuilder *build;
+    char *fileName;
+    GtkBuilder *builder;
 
-    build = gtk_builder_new_from_resource("/com/github/ztolley/editor/window.ui");
-    win = GTK_WIDGET (gtk_builder_get_object(build, "win"));
+    builder = gtk_builder_new_from_resource("/com/github/ztolley/editor/window.ui");
+    win = GTK_WIDGET (gtk_builder_get_object(builder, "win"));
     gtk_window_set_application(GTK_WINDOW (win), GTK_APPLICATION (app));
-    nb = GTK_WIDGET (gtk_builder_get_object(build, "nb"));
-    g_object_unref(build);
+    notebook = GTK_WIDGET (gtk_builder_get_object(builder, "notebook"));
+    g_object_unref(builder);
 
-    for (i = 0; i < n_files; i++) {
+    for (int i = 0; i < n_files; i++) {
         if (g_file_load_contents(files[i], NULL, &contents, &length, NULL, NULL)) {
-            scr = gtk_scrolled_window_new();
-            tv = tfe_text_view_new();
-            tb = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tv));
-            gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(tv), GTK_WRAP_WORD_CHAR);
-            gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scr), tv);
+            screen = gtk_scrolled_window_new();
+            textView = tfe_text_view_new();
+            textBuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
+            gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(textView), GTK_WRAP_WORD_CHAR);
+            gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(screen), textView);
 
-            tfe_text_view_set_file(TFE_TEXT_VIEW(tv), g_file_dup(files[i]));
-            gtk_text_buffer_set_text(tb, contents, length);
+            tfe_text_view_set_file(TFE_TEXT_VIEW(textView), g_file_dup(files[i]));
+            gtk_text_buffer_set_text(textBuffer, contents, length);
             g_free(contents);
 
-            filename = g_file_get_basename(files[i]);
-            lab = gtk_label_new(filename);
+            fileName = g_file_get_basename(files[i]);
+            label = gtk_label_new(fileName);
 
-            gtk_notebook_append_page(GTK_NOTEBOOK(nb), scr, lab);
-            nbp = gtk_notebook_get_page(GTK_NOTEBOOK(nb), scr);
+            gtk_notebook_append_page(GTK_NOTEBOOK(notebook), screen, label);
+            notebookPage = gtk_notebook_get_page(GTK_NOTEBOOK(notebook), screen);
 
-            g_object_set(nbp, "tab-expand", TRUE, NULL);
-            g_free(filename);
-        } else if ((filename = g_file_get_path(files[i])) != NULL) {
-            g_print("No such file: %s\n", filename);
-            g_free(filename);
+            g_object_set(notebookPage, "tab-expand", TRUE, NULL);
+            g_free(fileName);
+        } else if ((fileName = g_file_get_path(files[i])) != NULL) {
+            g_print("No such file: %s\n", fileName);
+            g_free(fileName);
         } else {
             g_print("No valid file is given\n");
         }
     }
 
-    if (gtk_notebook_get_pages(GTK_NOTEBOOK(nb)) > 0) {
+    if (gtk_notebook_get_pages(GTK_NOTEBOOK(notebook)) > 0) {
         gtk_widget_show(win);
     } else {
         gtk_window_destroy(GTK_WINDOW(win));
